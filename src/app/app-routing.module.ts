93d0aef79c18006e738d066423c2ba7ab0e58564
Thu Trang <94369431+Trangfpt@users.noreply.github.com>
2022-04-03 02:02:47 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { ClientComponent } from './client/client.component';
+import { DetailProductComponent } from './client/detail-product/detail-product.component';
+import { HomeComponent } from './client/home/home.component';
+import { ProductComponent } from './client/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductComponent
+      },
+      {
+        path: 'product/:id',
+        component: DetailProductComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+    ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ClientComponent } from './client/client.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { AsideComponent } from './layouts/aside/aside.component';
+import { HomeComponent } from './client/home/home.component';
+import { ProductComponent } from './client/product/product.component';
+import { DetailProductComponent } from './client/detail-product/detail-product.component';
+import { AdminComponent } from './admin/admin.component';
+import { CategoriesComponent } from './admin/categories/categories.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { UsersComponent } from './admin/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    HomeComponent,
+    ProductComponent,
+    DetailProductComponent,
+    AdminComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
